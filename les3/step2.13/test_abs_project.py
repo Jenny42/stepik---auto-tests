8e665a26631612aa66b8baa77aa4dd452333dee3
Jenny42 <jennydan@yandex.ru>
2021-04-21 10:59:41 +0700

    Раскиданы функции

--- les3/step2.13/test_abs_project.py
+++ les3/step2.13/test_abs_project.py
@@ -2,9 +2,17 @@ from selenium import webdriver
 import time
 import unittest
 
-def find_text(link):
-      browser = webdriver.Chrome()
+
+      
+class TestAbs(unittest.TestCase):
+    def setUp(self):
+      self.driver = webdriver.Chrome()
+    
+    def find_text(self, link):
+      browser = self.driver
       browser.get(link)
+      # говорим WebDriver ждать все элементы в течение 5 секунд
+      browser.implicitly_wait(5)
 
       # Ваш код, который заполняет обязательные поля
       input_first = browser.find_element_by_css_selector(".first:required") #псевдокласс
@@ -17,35 +25,24 @@ def find_text(link):
       # Отправляем заполненную форму
       button = browser.find_element_by_css_selector("button.btn")
       button.click()
-
-      # Проверяем, что смогли зарегистрироваться
-      # ждем загрузки страницы
-      time.sleep(1)   
-      # находим элемент, содержащий текст
-      welcome_text_elt = browser.find_element_by_tag_name("h1")  
-         
+      
       # записываем в переменную welcome_text текст из элемента welcome_text_elt
-      welcome_text = welcome_text_elt.text 
-      # ожидание чтобы визуально оценить результаты прохождения скрипта
-      time.sleep(5)
-      # закрываем браузер после всех манипуляций
-      browser.quit()
-      
-      return welcome_text
-      
-class TestAbs(unittest.TestCase):
-      
+      return browser.find_element_by_tag_name("h1").text      
     
     def test_abs1(self):
-      welcome_text = find_text("http://suninjuly.github.io/registration1.html")
+      welcome_text = self.find_text("http://suninjuly.github.io/registration1.html")
       # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
       self.assertEqual(welcome_text, "Congratulations! You have successfully registered!", "Sorry! You are not registered!")
 
      
     def test_abs2(self):
-      welcome_text2 = find_text("http://suninjuly.github.io/registration2.html")
+      welcome_text2 = self.find_text("http://suninjuly.github.io/registration2.html")
       # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
       self.assertEqual(welcome_text2, "Congratulations! You have successfully registered!", "Sorry! You are not registered!")
+      
+    def quitBrouser(self):
+      # закрываем браузер после всех манипуляций
+      self.driver.quit()
         
 if __name__ == "__main__":
     unittest.main()
